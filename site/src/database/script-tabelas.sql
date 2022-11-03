fa93844fa131082e5fa2dc0beb1775ae500f6b66
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-03 12:55:27 -0300

    Adição api web-data-viz

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,3 +1,4 @@
+-- Active: 1667002577304@@127.0.0.1@3306@pratica8
 -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
