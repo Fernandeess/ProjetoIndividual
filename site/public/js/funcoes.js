c649138dab0af88314cdb88b2245a4d7133b5f80
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-13 02:00:10 -0300

    alterações na api

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,16 +4,11 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
-    var b_usuario = document.getElementById("b_usuario");
-
+    console.log("validar")
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        b_usuario.innerHTML = nome;
-
-        // finalizarAguardar();
     } else {
-        window.location = "../index.html";
+        setTimeout(() => window.location = "../index.html",2000)
     }
 }
 
@@ -24,21 +19,22 @@ function limparSessao() {
     window.location = "../index.html";
 }
 
+
 // carregamento (loading)
 // function aguardar() {
 //     var divAguardar = document.getElementById("div_aguardar");
 //     divAguardar.style.display = "flex";
 // }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+// function finalizarAguardar(texto) {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.innerHTML = texto;
-    }
-}
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
 
 
 // modal
