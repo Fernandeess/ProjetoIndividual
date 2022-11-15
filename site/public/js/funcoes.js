ede7831f92197afb5c5e68ea4de57b098b8221ff
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-15 03:50:02 -0300

    funçoes modificadas

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -20,33 +20,11 @@ function limparSessao() {
     // finalizarAguardar();
     window.location = "../index.html";
 }
-
-
-// carregamento (loading)
-// function aguardar() {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "flex";
-// }
-
-// function finalizarAguardar(texto) {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "none";
-
-//     var divErrosLogin = document.getElementById("div_erros_login");
-//     if (texto) {
-//         divErrosLogin.innerHTML = texto;
-//     }
-// }
-
+function limparErros(){
+    div_aguardar.style = `display:none;`
+    div_erros_login.style = `display:none;`
+    console.log("fechar loading")
+}
 
 // modal
-function mostrarModal() {
-    var divModal = document.getElementById("div_modal");
-    divModal.style.display = "flex";
-}
-
-function fecharModal() {
-    var divModal = document.getElementById("div_modal");
-    divModal.style.display = "none";
-}
 
