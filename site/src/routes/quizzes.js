06c9ea27fdb6d415499a41d8f3dca63b9ca7530d
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-27 23:16:37 -0300

    criei a rota do quiz

--- site/src/routes/quizzes.js
+++ site/src/routes/quizzes.js
@@ -6,4 +6,7 @@ var quizController = require("../controllers/quizController");
 router.post("/listarRankingQuiz1", function(req,res){
     quizController.listarRankingQuiz1(req,res)
 })
+router.put("/insertPoints", function(req,res){
+    quizController.insertPoints(req,res)
+})
 module.exports = router;
